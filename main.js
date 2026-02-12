8d14e312f832511d80bad476d881eb2f37f772af
io.FortuNato <nomefortunato60@gmail.com>
2026-02-12 01:01:09 +0100

    first task complete

---
+++ main.js
@@ -0,0 +1,8 @@
+
+let lampadina = document.querySelector(".lampadina");
+let btn = document.querySelector(".btn");
+
+btn.addEventListener("click",
+    function() {
+    lampadina.src = "img/yellow_lamp.png";
+});
