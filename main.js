f5def228d75c90600c4e6a7434a48748d5a45a30
io.FortuNato <nomefortunato60@gmail.com>
2026-02-12 01:12:06 +0100

    add bonus complete

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ let lampadina = document.querySelector(".lampadina");
 let btn = document.querySelector(".btn");
 
 btn.addEventListener("click",
-    function() {
-    lampadina.src = "img/yellow_lamp.png";
-});
+    function () {
+
+        if (lampadina.src.includes("white_lamp")) {
+            lampadina.src = "img/yellow_lamp.png";
+            btn.textContent = "spegni";
+        } else {
+            lampadina.src = "img/white_lamp.png";
+            btn.textContent = "accendi";
+        }
+    });
